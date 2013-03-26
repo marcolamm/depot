0ae61b76daac58f3f6cd5be9703263662fdd9b18
marcolamm <marcolamm68@gmail.com>
2013-03-26 23:35:01 +0100

    swiss format price

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,20 @@
 class Product < ActiveRecord::Base
   attr_accessible :available, :description, :image_url, :price, :title
-  validates_presence_of :title, :description, :price
-   # validation_numerically_of : price
+	validates_presence_of :title, :description #:price
+    validates_numericality_of :price 
+	validates :price, :numericality => {:greater_than_or_equal_to => 0.05}
+	validates :price, :format => { :with => /^\d+(\.[0-9][0|5]?)?$/}
+											#/^(\d+)(\.\d(05)?)?$/
+	validates :image_url, allow_blank: true, format: {
+	with: %r{\.(gif|jpg|png)\Z}i,
+	message: 'must be a URL for GIF, JPG or PNG image.'
+	}
+	
+	#validate :validate_swissPrice
+	
+	# protected
+	# def validate_swissPrice
+    # return if price = 1 
+    # errors.add(:fields, "Betrag auf .05 Rappen eingeben")
+  # end
 end
