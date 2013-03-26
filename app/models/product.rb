2bc917f5398d843812984202f8dd53934f823d8e
marcolamm <marcolamm68@gmail.com>
2013-03-27 00:10:27 +0100

    tests

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
 	validates_presence_of :title, :description #:price
     validates_numericality_of :price 
 	validates :price, :numericality => {:greater_than_or_equal_to => 0.05}
-	validates :price, :format => { :with => /^\d+(\.[0-9][0|5]?)?$/}
+	validates :price, :format => { :with => /^\d+(\.[0-9][0|5]?)?$/,  :message => 'Must be  format e.g. 45.05 - smallest unit is 5 cents!!'}
 											#/^(\d+)(\.\d(05)?)?$/
 	validates :image_url, allow_blank: true, format: {
 	with: %r{\.(gif|jpg|png)\Z}i,
