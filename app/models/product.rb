35588d85126d2f5cd14d508fde04d1d4df19058b
marcolamm <marcolamm68@gmail.com>
2013-04-30 15:52:12 +0200

    MengenZu Artikel

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,15 @@
 class Product < ActiveRecord::Base
-  attr_accessible :available, :description, :image_url, :price, :title
+  attr_accessible :available, :description, :image_url, 
+:price, :title
 	validates_presence_of :title, :description #:price
     validates_numericality_of :price 
-	validates :price, :numericality => {:greater_than_or_equal_to => 0.05}
-	validates :price, :format => { :with => /^\d+(\.[0-9][0|5]?)?$/,  :message => 'Must be  format e.g. 45.05 - smallest unit is 5 cents!!'}
-											#/^(\d+)(\.\d(05)?)?$/
+	validates :price, :numericality => {
+:greater_than_or_equal_to => 0.05}
+	validates :price, :format => { :with => 
+/^\d+(\.[0-9][0|5]?)?$/,  :message => 'Must be  
+format e.g. 45.05 - smallest unit is 5 cents!!'}
+											
+#/^(\d+)(\.\d(05)?)?$/
 	validates :image_url, allow_blank: true, format: {
 	with: %r{\.(gif|jpg|png)\Z}i,
 	message: 'must be a URL for GIF, JPG or PNG image.'
