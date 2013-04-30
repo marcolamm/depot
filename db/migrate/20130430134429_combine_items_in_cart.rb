e8679308a8a0280f9e66588ae009871e51b718eb
marcolamm <marcolamm68@gmail.com>
2013-05-01 00:18:16 +0200

    update1

--- db/migrate/20130430134429_combine_items_in_cart.rb
+++ db/migrate/20130430134429_combine_items_in_cart.rb
@@ -11,7 +11,7 @@ if quantity > 1
 cart.line_items.where(:product_id=>product_id).
 delete_all
 # replace with a single item
-cart.line_items.create(:product=>product, 
+cart.line_items.create(:product_id=>product_id, 
 :quantity=>quantity)
 end
 end
