6af30eceb8e709cebecf2e2bf6b791e247d35367
marcolamm <marcolamm68@gmail.com>
2013-06-04 14:22:06 +0200

    up

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,8 +2,8 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.order('title ASC')
-
+    #@products = Product.order('title ASC')
+   @products = Product.all
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @products }
